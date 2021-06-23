743d10f7716266de652f17cc7721e53aad9ba616
Ankit-akumar <as2262011@gmail.com>
2021-06-23 18:10:51 +0530

    results activity with firebase and implemented update user score

--- app/src/main/java/com/example/quizoholic/ResultActivity.kt
+++ app/src/main/java/com/example/quizoholic/ResultActivity.kt
@@ -3,17 +3,22 @@ package com.example.quizoholic
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.quizoholic.databinding.ActivityResultBinding
+import com.example.quizoholic.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
     private var correctResponses = 0
     private var incorrectResponses = 0
     private var noResponse = 0
-//    private lateinit var mDatabase: FirebaseDatabase
-//    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: FirebaseDatabase
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,23 +34,38 @@ class ResultActivity : AppCompatActivity() {
         binding.tvIncorrect.text = incorrectResponses.toString()
         binding.tvUnattempted.text = noResponse.toString()
 
-//        mDatabase = FirebaseDatabase.getInstance()
-//        mAuth = FirebaseAuth.getInstance()
-//        setUserScore()
+        mDatabase = FirebaseDatabase.getInstance()
+        mAuth = FirebaseAuth.getInstance()
 
         binding.btnPlayAgain.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+        val currentUser = mAuth.currentUser
+        var oldScore = "0"
+        var isDone = false
+
+        mDatabase.reference.child("Users").child(currentUser!!.uid).child("totalScore")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    oldScore = snapshot.value.toString()
+                    println("Old score = $oldScore")
+                    if(!isDone) {
+                        setUserScore(oldScore)
+                        isDone = true
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w("LoadUserError", "loadUser:onCancelled", error.toException())
+                }
+            })
     }
 
-//    private fun setUserScore() {
-//        val currentUser = mAuth.currentUser
-//        val oldScore =
-//            mDatabase.reference.child("Users").child(currentUser!!.uid).child("totalScore")
-//                .toString()
-//        val newScore = (oldScore.toInt() + correctResponses).toString()
-//        mDatabase.reference.child("Users").child(currentUser.uid).child("totalScore")
-//            .setValue(newScore)
-//    }
+    private fun setUserScore(oldScore:String) {
+        val newScore = (oldScore.toInt() + correctResponses).toString()
+        mDatabase.reference.child("Users").child(mAuth.currentUser!!.uid).child("totalScore")
+            .setValue(newScore)
+    }
 }
--- app/src/main/java/com/example/quizoholic/UserProfile.kt
+++ app/src/main/java/com/example/quizoholic/UserProfile.kt
@@ -20,37 +20,26 @@ class UserProfile : AppCompatActivity() {
         setContentView(binding.root)
 
         mAuth = FirebaseAuth.getInstance()
-        this.database = FirebaseDatabase.getInstance().reference
+        database = FirebaseDatabase.getInstance().reference
 
-        val firebaseUser = mAuth.currentUser
+        database.child("Users").child(mAuth.currentUser!!.uid)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    // Get User object and use the values to update the UI
+                    val user = snapshot.getValue(User::class.java)
 
-        val user = User(
-            username = firebaseUser!!.displayName.toString(),
-            bestScore = database.child("Users").child(firebaseUser.uid)
-                .child("bestScore").toString(),
-            totalScore = this.database.child("Users").child(firebaseUser.uid)
-                .child("totalScore").toString(),
-            profilePic = firebaseUser.photoUrl.toString()
-        )
+                    binding.tvUsername.text = user!!.username
+                    Picasso.get().load(user.profilePic)
+                        .placeholder(R.drawable.ic_baseline_account_circle_24)
+                        .into(binding.profileImage)
+                    binding.tvBestScore.text = user.bestScore
+                    binding.tvTotalScore.text = user.totalScore
+                }
 
-        binding.tvUsername.text = user.username
-        Picasso.get().load(user.profilePic)
-            .placeholder(R.drawable.ic_baseline_account_circle_24)
-            .into(binding.profileImage)
-
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get User object and use the values to update the UI
-                val userT: User? = dataSnapshot.getValue(User::class.java)
-                binding.tvBestScore.text = userT!!.bestScore
-                binding.tvTotalScore.text = userT.totalScore
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("TAG", "loadPost:onCancelled", databaseError.toException())
-            }
-        }
-        database.addValueEventListener(postListener)
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Getting Post failed, log a message
+                    Log.w("UserProfile", "loadPost:onCancelled", databaseError.toException())
+                }
+            })
     }
 }
